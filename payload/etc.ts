db36369a3fedbdb18c9708c6e17274097803eaa7
prs-tl <prs0228@topialive.co.kr>
2025-06-23 14:33:14 +0900

    bug fix

--- payload/etc.ts
+++ payload/etc.ts
@@ -3,15 +3,7 @@ import { IEtc } from '../component/etc/IEtc';
 const etc: IEtc.Payload = {
   disable: false,
 
-  list: [
-    {
-      title: '취미',
-      descriptions: [
-        '책 읽기',
-        '영화 보기',
-      ],
-    },
-  ],
+  list: [],
 };
 
 export default etc;
